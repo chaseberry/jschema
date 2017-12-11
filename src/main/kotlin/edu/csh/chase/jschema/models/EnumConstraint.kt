4d8e9a64e17438c921b0db1307ec1fcbbcde5a92
Chase <chase@csh.rit.edu>
2017-12-10 19:07:30 -0500

    Using helper functions from parent obj

--- src/main/kotlin/edu/csh/chase/jschema/models/EnumConstraint.kt
+++ src/main/kotlin/edu/csh/chase/jschema/models/EnumConstraint.kt
@@ -1,18 +1,17 @@
 package edu.csh.chase.jschema.models
 
 import edu.csh.chase.jschema.JSchemaConfig
-import edu.csh.chase.jschema.JSchemaUtils
 
 class EnumConstraint<T : List<Any?>>(value: T, config: JSchemaConfig) : Constraint<T>("enum", value, config) {
 
     override fun checkSet() {
         value?.let {
             if (it.isEmpty()) {
-                JSchemaUtils.warn("`enum` value should contain at least 1 element", config)
+                warn("`enum` value should contain at least 1 element")
             }
 
             if (it.intersect(it).isNotEmpty()) {
-                JSchemaUtils.warn("`enum` value should be an array of unique values", config)
+                warn("`enum` value should be an array of unique values")
             }
         }
     }
