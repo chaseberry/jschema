ffa9fde53918aa54945dc4ee91e70536b128c563
Chase <chase@csh.rit.edu>
2017-12-10 19:29:53 -0500

    Added config to treat format as a Constraint instead of an annotation

--- src/main/kotlin/edu/csh/chase/jschema/JSchemaConfig.kt
+++ src/main/kotlin/edu/csh/chase/jschema/JSchemaConfig.kt
@@ -1,5 +1,5 @@
 package edu.csh.chase.jschema
 
 data class JSchemaConfig(val errOnFailure: Boolean = false, val errOnWarn: Boolean = false,
-                         val debugEnabled: Boolean = false) {
+                         val debugEnabled: Boolean = false, val formatIsConstraint: Boolean = true) {
 }
