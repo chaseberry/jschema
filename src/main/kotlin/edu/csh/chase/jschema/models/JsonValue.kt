2d13a34a397c7ced1c2a84dc8c8ac8c6c67b8400
Chase <chase@csh.rit.edu>
2017-12-25 16:40:15 -0500

    Fixed types for Number, Added a from function to create a JsonValue or null

--- src/main/kotlin/edu/csh/chase/jschema/models/JsonValue.kt
+++ src/main/kotlin/edu/csh/chase/jschema/models/JsonValue.kt
@@ -6,13 +6,27 @@ import edu.csh.chase.kjson.JsonObject
 sealed class JsonValue(open val value: Any?) {
 
     class Boolean(override val value: Boolean) : JsonValue(value)
-    sealed class Number(override val value: Number) : JsonValue(value) {
-        class Int(override val value: Int) : Number(value)
-        class Double(override val value: Double) : Number(value)
+    sealed class Number(override val value: kotlin.Number) : JsonValue(value) {
+        class Int(override val value: kotlin.Int) : Number(value)
+        class Double(override val value: kotlin.Double) : Number(value)
     }
 
     class String(override val value: String) : JsonValue(value)
     class Array(override val value: JsonArray) : JsonValue(value)
     class Object(override val value: JsonObject) : JsonValue(value)
     class Null : JsonValue(null)
+
+    companion object {
+        fun from(v: Any?): JsonValue? = when (v) {
+            null -> Null()
+            is Boolean -> Boolean(v)
+            is Int -> Number.Int(v)
+            is Double -> Number.Double(v)
+            is String -> String(v)
+            is JsonObject -> Object(v)
+            is JsonArray -> Array(v)
+            else -> null
+        }
+    }
+
 }
