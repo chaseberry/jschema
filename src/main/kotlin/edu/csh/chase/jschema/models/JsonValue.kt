fb563d36ae7eb836ba2339f6699488e238e2e960
Chase <chase@csh.rit.edu>
2017-12-24 19:52:10 -0500

    Gave the JsonValues a single base var to subtype

--- src/main/kotlin/edu/csh/chase/jschema/models/JsonValue.kt
+++ src/main/kotlin/edu/csh/chase/jschema/models/JsonValue.kt
@@ -3,16 +3,16 @@ package edu.csh.chase.jschema.models
 import edu.csh.chase.kjson.JsonArray
 import edu.csh.chase.kjson.JsonObject
 
-sealed class JsonValue {
+sealed class JsonValue(open val value: Any?) {
 
-    class Boolean(val boolean: Boolean) : JsonValue()
-    sealed class Number(open val num: Number) : JsonValue() {
-        class Int(override val num: Int) : Number(num)
-        class Double(override val num: Double) : Number(num)
+    class Boolean(override val value: Boolean) : JsonValue(value)
+    sealed class Number(override val value: Number) : JsonValue(value) {
+        class Int(override val value: Int) : Number(value)
+        class Double(override val value: Double) : Number(value)
     }
 
-    class String(val str: String) : JsonValue()
-    class Array(val arr: JsonArray) : JsonValue()
-    class Object(val obj: JsonObject) : JsonValue()
-
+    class String(override val value: String) : JsonValue(value)
+    class Array(override val value: JsonArray) : JsonValue(value)
+    class Object(override val value: JsonObject) : JsonValue(value)
+    class Null : JsonValue(null)
 }
