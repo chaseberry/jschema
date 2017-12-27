16e88280ade36f6bf1a087cc44abf0596816fc65
Chase <chase@csh.rit.edu>
2017-12-27 09:52:55 -0500

    Added a possible basic concept for a Parser

--- src/main/kotlin/edu/csh/chase/jschema/parser/ConstraintParser.kt
+++ src/main/kotlin/edu/csh/chase/jschema/parser/ConstraintParser.kt
@@ -1,5 +1,7 @@
 package edu.csh.chase.jschema.parser
 
+import edu.csh.chase.jschema.JSchemaConfig
+import edu.csh.chase.jschema.models.JsonValue
 import edu.csh.chase.jschema.models.constraints.Constraint
 
-class ConstraintParser(val name: String, parse: (Any?) -> Constraint<*>)
+class ConstraintParser<T : Constraint<*>>(parse: (JsonValue, JSchemaConfig) -> T)
