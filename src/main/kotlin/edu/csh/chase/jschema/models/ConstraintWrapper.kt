4e8f1f66f92685ce72e0320874cc22b8cd7c0152
Chase <chase@csh.rit.edu>
2017-12-27 10:04:29 -0500

    Added the Serializer to the wrapper

--- src/main/kotlin/edu/csh/chase/jschema/models/ConstraintWrapper.kt
+++ src/main/kotlin/edu/csh/chase/jschema/models/ConstraintWrapper.kt
@@ -2,6 +2,7 @@ package edu.csh.chase.jschema.models
 
 import edu.csh.chase.jschema.models.constraints.Constraint
 import edu.csh.chase.jschema.parser.ConstraintParser
+import edu.csh.chase.jschema.parser.ConstraintSerializer
 
-class ConstraintWrapper<T : Constraint<*>>(val name: String, val clazz: Class<T>, parser: ConstraintParser<T>) {
+class ConstraintWrapper<T : Constraint<*>>(val name: String, val clazz: Class<T>, parser: ConstraintParser<T>, serializer: ConstraintSerializer<T>) {
 }
